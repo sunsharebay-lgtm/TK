c1fde5df15db2d3cd4bf7e253f1bdb07bf48b5f0
sunsharebay-lgtm <sunsharebay@gmail.com>
2026-08-27 03:13:26 +0800

    test/docs: 技能菜单非战斗施法用例(赤心计回血扣谋点)allOk, 单元10

--- 吞食天地三国/测试/tools/regress.mjs
+++ 吞食天地三国/测试/tools/regress.mjs
@@ -22,6 +22,7 @@ const CASES = [
   { name: "storage",     url: "http://localhost:8642/?autostart&goto=23,114,141", dur: 6000, keys: "Enter:400:2", pre: "cases/storage.pre.json", end: "cases/storage.end.json" },
   { name: "shop",         url: "http://localhost:8642/?autostart&goto=23,114,141", dur: 5000, keys: "",                  pre: "cases/shop.pre.json",       end: "cases/shop.end.json" },
   { name: "title-continue", url: "http://localhost:8642/?autostart&goto=23,114,141", dur: 12000, pre: "cases/title-continue-a.pre.json", end: "cases/title-continue-b.end.json", mid: "cases/title-continue.mid.json", midAt: 4000 },
+  { name: "skill-menu",  url: "http://localhost:8642/?autostart&goto=23,114,141", dur: 6000, keys: "", pre: "cases/skill-menu.pre.json", end: "cases/skill-menu.end.json" },
   { name: "equip-8slot", url: "http://localhost:8642/?autostart&autointro=18&goto=23,114,141", dur: 12000, keys: "Enter:300:4", pre: "cases/equip-8slot.pre.json", end: "cases/equip-8slot.end.json", custom: "equip8" },
 ];
 
